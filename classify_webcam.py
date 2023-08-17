ffd33feefad9690678390c678f052d6addfcedfd
PranavPradhan <pranav.pradhan1@gmail.com>
2023-08-16 20:33:54 -0400

    training model

---
+++ classify_webcam.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import cv2
+ 
+ 
--- model.py
+++
@@ -1,8 +0,0 @@
-import math
-import numpy 
-import tensorflow as tf 
-from keras.preprocessing.image import ImageDataGenerator
-
-print(tf.__version__ ) 
- 
- 
