7c396cc2794cb5c119a12ed5da91bf1bfad4bf14
PranavPradhan <pranav.pradhan1@gmail.com>
2023-12-22 18:00:11 -0500

    working model

--- classify_webcam.py
+++ classify_webcam.py
@@ -6,5 +6,99 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 import cv2
- 
- 
+import json
+from keras.models import load_model
+from keras.models import model_from_json
+
+# Load the architecture from the JSON file
+with open("training/model_architecture.json", "r") as f:
+    architecture = f.read()
+
+# Create the model from the loaded architecture
+model = model_from_json(architecture)
+
+# Load the weights from the .h5 file
+model.load_weights("training/model_weights.h5")
+
+import time
+
+
+def keras_predict(model, image):
+    pred_probab = model.predict(processed)[0]
+    pred_class = list(pred_probab).index(max(pred_probab))
+    return max(pred_probab), pred_class
+
+
+def recognize():
+    cap = cv2.VideoCapture(0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    scale = 1
+    if cap.read()[0] == False:
+        cap = cv2.VideoCapture(0)
+
+
+# Define the ASL symbols
+asl_symbols = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+
+# Set the font and scale for displaying the ASL symbol
+font = cv2.FONT_HERSHEY_SIMPLEX
+scale = 1
+
+# Run the webcam application
+while True:
+    # Read the frame from the webcam
+    ret, frame = cap.read()
+
+    # Preprocess the frame
+    # ...YOUR_CODE...
+
+    # Perform the classification
+    # ...YOUR_CODE...
+    prediction = model.predict(preprocessed_frame)
+    predicted_symbol = asl_symbols[np.argmax(prediction)]
+
+    # Display the ASL symbol on the frame
+    cv2.putText(
+        frame, predicted_symbol, (10, 50), font, scale, (0, 255, 0), 2, cv2.LINE_AA
+    )
+
+    # Show the frame
+    cv2.imshow("ASL Classification", frame)
+
+    # Check for the 'q' key to exit the application
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# Release the webcam and close all windows
+cap.release()
+cv2.destroyAllWindows()
